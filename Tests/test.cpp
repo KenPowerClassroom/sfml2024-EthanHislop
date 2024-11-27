36c79f540a260d740c3f7d823b4b6921a4219f0b
(Student  C00296735) Ethan Hislop <C00296735@setu.ie>
2024-11-27 14:47:14 +0000

    Test 2, 3 and 4 Done

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -14,6 +14,7 @@
 
 #include "../16_SFML_Games/Block.h"
 
+// Test to see if setBlockPosition sets the x and y pos correctly
 TEST(Block, SetBlockPos) {
 
 	Block block;
@@ -22,4 +23,36 @@ TEST(Block, SetBlockPos) {
 
 	EXPECT_EQ(block.getBody().getPosition().x, 10);
 	EXPECT_EQ(block.getBody().getPosition().y, 10);
+}
+
+// Test to see if getting the body-position gives the right set position
+TEST(Block, GetSpritePos) {
+
+	Block block2;
+
+	block2.setBlockPosition(10, 10);
+
+	Vector2f vector = block2.getBody().getPosition();
+
+	EXPECT_EQ(vector, Vector2f(10, 10)); 
+}
+
+// Test to get the block object width
+TEST(Block, GetSpriteWidth) {
+
+	Block block3;
+
+	int width = block3.getBody().getGlobalBounds().width;
+	
+	EXPECT_EQ(width, 0);
+}
+
+// Test to see if a block is colliding with a ball
+TEST(Block, CheckBlockCollision) {
+
+	Block block4;
+
+	block4.setBlockPosition(10, 10);
+
+	EXPECT_EQ(block4.checkBlockCollision(5, 10, 10), 5);
 }
